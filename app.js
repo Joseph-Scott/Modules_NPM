31e017723ef630c57091a8b559d5f6c941cb289a
Joe Scott <jbs198585@gmail.com>
2022-03-16 06:31:22 -0400

    package.json

---
+++ app.js
@@ -0,0 +1,9 @@
+const { PI, square } = require('./math');
+
+const cats = require('./shelter')
+
+// console.log(math.PI);
+
+// console.log(math.square(9))
+
+console.log("REQUIRED AN ENTIRE DIRECTORY!", cats);
